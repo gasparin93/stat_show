1f7f96f260d442fa85a28e097d2103520bbab2fc
Edgar Sias <eivansias@gmail.com>
2025-01-31 20:20:03 -0600

    changing annotations to remove Data, removing fat-finger string

--- src/main/java/com/honestefforts/stat_show/controller/PaymentController.java
+++ src/main/java/com/honestefforts/stat_show/controller/PaymentController.java
@@ -7,7 +7,7 @@ import com.honestefforts.stat_show.service.PaymentProducer;
 import java.util.Comparator;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;stat_show
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/com/honestefforts/stat_show/model/PaymentStats.java
+++ src/main/java/com/honestefforts/stat_show/model/PaymentStats.java
@@ -1,12 +1,15 @@
 package com.honestefforts.stat_show.model;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
 @AllArgsConstructor
-@Data
-@FieldDefaults(makeFinal = true)
+@Getter
+@Builder
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class PaymentStats {
   String user;
   long transactionCount;
